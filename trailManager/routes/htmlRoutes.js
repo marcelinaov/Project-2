be7b19e4e02245751eaa0b0cfb0ba821cb93c864
Christa <christahyde@yahoo.com>
2019-05-24 21:29:10 -0600

    Updated functionality on search

--- trailManager/routes/htmlRoutes.js
+++ trailManager/routes/htmlRoutes.js
@@ -14,28 +14,32 @@ module.exports = function (app) {
 
   app.get("/trail", function (req, res) {
     // console.log(req.query);
-    var minLengthValue = req.query.minLengthInput;
-    if (!minLengthValue) {
-      minLengthValue = '10'
-    }
+    const availableHours = req.query.availableHours || 10;
     // Salt Lake City 40.7608° N, 111.8910° W
     const apiKey = encodeURIComponent('200472533-7cad91d48c212e1fc15dd52e7b19b2ad');
     const lat = encodeURIComponent('40.7608');
     const long = encodeURIComponent('-111.8910');
     const maxDistance = encodeURIComponent('100');
     const maxResults = encodeURIComponent('100');
-    const minLength = encodeURIComponent(minLengthValue);
     const apiSort = 'quality';
-    const apiUrl = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=${maxDistance}&maxResults=${maxResults}&minLength=${minLength}&sort=${apiSort}&key=${apiKey}`;
+    // Calculate minLength value based on available hours, estimating walking speed of 2.5
+    const maxLength = availableHours * 2.5;
+    const apiUrl = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=${maxDistance}&maxResults=${maxResults}&sort=${apiSort}&key=${apiKey}`;
     console.log(apiUrl);
     // TODO: Tranform the data to remove entries below the maximum length we require, which is not provided by the API
     // Pull data from Trails API
     // https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200472533-7cad91d48c212e1fc15dd52e7b19b2ad
     axios.get(apiUrl)
       .then(function (response) {
-
+        // Process the response data, removing any trails with a length longer than our max
+        // const trails = response.data.trails.filter(entry => entry.length <= maxLength);
+        const trails = [];
+        for (entry of response.data.trails) {
+          if (entry.length <= maxLength) trails.push(entry);
+        }
+        // trails.sort((a, b) => +a.length + +b.length);
         // console.log(response.data.trails);
-        res.render("trail", response.data);
+        res.render("trail", { trails: trails });
       })
       .catch(function (err) {
         console.log(err);
